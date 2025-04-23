080abbdc3f4a352f979607c678dba1914d0dec3e
Lethabarb <colininja@outlook.com.au>
2025-04-23 20:04:15 +1000

    Add project files.

---
+++ ParcellPartnersTechnicalTest/ArrayEvaluator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcellPartnersTechnicalTest
+{
+    public class ArrayEvaluator
+    {
+        public int FindMissingNumber(int[] ints)
+        {
+            List<int> list = ints.ToList();
+            list.Sort();
+
+            int first = list.First();
+            int last = list.Last();
+
+            //this needs to be checked as the following algorithm will not work unless a middle number is missing
+            if (first > 0) return 0;
+            if (last != list.Count) return list.Count;
+
+            // sum the set and find the expected sum
+            // since we know the first and the last number of the set we can calculate the expected sum
+            // using the arithmic sequence sum formula.
+            double sum = Sum(ints);
+            double expected = (list.Count + 1) / 2 * last;
+
+            // the missing number can be found by subtracting the expected from the actual sum.
+            // NOTE: this will ONLY work if 1 element is missing from the set. 
+            double missing = expected - sum;
+            return (int) missing;
+
+        }
+
+
+        private double Sum(int[] ints)
+        {
+            double sum = 0;
+            for (int i = 0; i < ints.Length; i++)
+            {
+                sum += ints[i];
+            }
+            return sum;
+        }
+
+
+    }
+}
---
+++ ParcellPartnersTechnicalTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using ParcellPartnersTechnicalTest;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        args = ["9", "6", "4", "2", "3", "5", "7", "0", "1"];
+        int[] set = new int[args.Length];
+
+        //convert string args into a ints
+        for (int i = 0; i < set.Length; i++) {
+            string input = args[i];
+            int number = int.Parse(input);
+            set[i] = number;
+        }
+
+        //find the missing number
+        ArrayEvaluator eval = new ArrayEvaluator();
+        int missing = eval.FindMissingNumber(set);
+
+        Console.WriteLine(missing);
+
+    }
+}
